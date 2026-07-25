e83a5a0229ed2184a288265376012d8b8bc80a10
Junior Kongne <93330123+kongne@users.noreply.github.com>
2026-07-25 11:08:04 +0100

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,72 @@
+import inertia from '@inertiajs/vite';
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
+            refresh: true,
+            fonts: [
+                bunny('Instrument Sans', {
+                    weights: [400, 500, 600],
+                }),
+            ],
+        }),
+
+        VitePWA({
+            registerType: 'autoUpdate',
+
+            includeAssets: [
+                'favicon.svg',
+                'favicon.ico',
+                'robots.txt',
+                'apple-touch-icon.png',
+            ],
+            manifest: {
+                name: 'Pulse',
+                short_name: 'Pulse',
+                description: 'A Laravel Inertia Starter Kit',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: '/pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+            },
+        }),
+
+        inertia(),
+        tailwindcss(),
+
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        wayfinder({
+            formVariants: true,
+        }),
+    ],
+});
